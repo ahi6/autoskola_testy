0d76ff56e69040e13de31402de814c76a35594bc
ahi6 <60984726+ahi6@users.noreply.github.com>
2025-07-15 19:15:41 +0200

    Make QuestionView display a question

--- src/views/question_view.rs
+++ src/views/question_view.rs
@@ -4,60 +4,31 @@ use dioxus::prelude::*;
 use downloader4etesty2::types::*;
 
 use crate::components::TestQuestion;
-use crate::BASE_URL;
 
 #[component]
 pub fn QuestionView(topic_title: String) -> Element {
-    let placeholder_question = Question::default();
-
-    static PATH_TO_QUESTIONS: Asset = asset!("/assets/output/Znalost dopravních znače.json");
-
-    let questions_fut: Resource<Vec<Question>> = use_resource(move || async move {
-        let questions_json = reqwest::get(format!("{}{}", BASE_URL, PATH_TO_QUESTIONS))
-            .await
-            .expect("Failed to fetch questions")
-            .text()
-            .await
-            .expect("Failed to read response text");
-        serde_json::from_str(&questions_json).expect("Failed to parse JSON")
-    });
-
     let mut questions_signal: Signal<Option<Result<Vec<Question>, String>>> =
         dioxus_sdk::storage::use_synced_storage::<
             dioxus_sdk::storage::LocalStorage,
             Option<Result<Vec<Question>, String>>,
         >(calculate_hash(&topic_title).to_string(), || None);
 
-    // let question_rsx = match &*questions_fut.read_unchecked() {
-    //     Some(questions) => {
-    //         let question = questions
-    //             .get(0)
-    //             .cloned()
-    //             .unwrap_or(placeholder_question.clone());
-    //         rsx! {
-    //             TestQuestion { question }
-    //         }
-    //     }
-    //     None => {
-    //         rsx! {
-    //             p {
-    //                 "No questions available"
-    //             }
-    //         }
-    //     }
-    // }
-    // .unwrap();
-    //
+    let question_index = use_hook(|| 0 as usize);
+
     let questions_cloned = questions_signal.read().clone();
     let question_rsx = match questions_cloned {
-        Some(Ok(questions)) => rsx! {
-            p {
-                "Questions loaded"
+        Some(Ok(questions)) => {
+            let question = questions
+                .get(question_index)
+                .cloned()
+                .unwrap_or(Question::default());
+
+            rsx! {
+                TestQuestion {
+                    question
+                }
             }
-            p {
-                {format!("{:?}", questions)}
-            }
-        },
+        }
         Some(Err(error_msg)) => rsx! {
             p {
                 "An error occurred while loading questions:"
